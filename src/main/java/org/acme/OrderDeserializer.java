1fb2a24fdc8920d95fb3578d3051d000f00ce226
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-04-18 11:55:49 -0300

    versao com shared

---
+++ src/main/java/org/acme/OrderDeserializer.java
@@ -0,0 +1,11 @@
+package org.acme;
+
+import org.acme.domain.Order;
+
+import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
+
+public class OrderDeserializer extends ObjectMapperDeserializer<Order> {
+    public OrderDeserializer() {
+        super(Order.class);
+    }
+}
--- src/main/java/org/acme/OrderReceiver.java
+++ src/main/java/org/acme/OrderReceiver.java
@@ -1,6 +1,7 @@
 package org.acme;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
 import org.acme.domain.Order;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -9,6 +10,7 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 public class OrderReceiver {
   
     @Incoming("orders-in")
+    @Transactional
     public void consume(Order order) {
         System.out.println(order.getCustomer());
     }
