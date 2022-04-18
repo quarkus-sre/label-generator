ec8eb9aea743210f37ea1b7a1ce53f2fa2f3bc6b
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-04-18 11:33:58 -0300

    v1 do label generator

--- src/main/java/org/acme/OrderReceiver.java
+++ src/main/java/org/acme/OrderReceiver.java
@@ -1,5 +1,16 @@
 package org.acme;
 
+import javax.enterprise.context.ApplicationScoped;
+
+import org.acme.domain.Order;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+@ApplicationScoped
 public class OrderReceiver {
   
+    @Incoming("orders-in")
+    public void consume(Order order) {
+        System.out.println(order.getCustomer());
+    }
+    
 }
