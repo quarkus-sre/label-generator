52a91eb51f57ef9f37fdcd4a09704a1021f25098
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-05-09 14:43:19 -0300

    adicionando micrometer

--- src/main/java/com/redhat/quarkus/sre/OrderReceiver.java
+++ src/main/java/com/redhat/quarkus/sre/OrderReceiver.java
@@ -1,5 +1,8 @@
 package com.redhat.quarkus.sre;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -8,6 +11,8 @@ import com.redhat.quarkus.sre.sender.OrderPackageSender;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import io.smallrye.reactive.messaging.annotations.Blocking;
 
 @ApplicationScoped
@@ -15,12 +20,17 @@ public class OrderReceiver {
 
     @Inject
     private OrderPackageSender sender;
+
+    @Inject
+    MeterRegistry registry;
   
     @Incoming("orders-in")
     @Blocking
     public void consume(Order order) {
+        Timer timer = registry.timer("com.redhat.quarkus.sre.tempo.consumo");
         System.out.println("OrderReceiver.consume()");
         sender.send(order);
+        timer.record(Duration.between(order.getCreationDateTime(), LocalDateTime.now()));
     }
     
 }
