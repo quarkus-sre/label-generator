5b2b006def732bb52b7599ac912dd3e306f723d5
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-05-24 10:45:05 -0300

    contador da fedex e ups

--- src/main/java/com/redhat/quarkus/sre/label/sender/FedexOrderPackageSender.java
+++ src/main/java/com/redhat/quarkus/sre/label/sender/FedexOrderPackageSender.java
@@ -1,11 +1,15 @@
 package com.redhat.quarkus.sre.label.sender;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import com.redhat.quarkus.sre.label.domain.Order;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.opentracing.Traced;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @ApplicationScoped
 @Traced
@@ -13,6 +17,12 @@ public class FedexOrderPackageSender {
 
     @ConfigProperty(name = "fedex.delay", defaultValue = "1000")
     Integer delay;
+
+    private Counter counter;
+
+    FedexOrderPackageSender(MeterRegistry registry) {
+        this.counter = registry.counter("sre.label-generator.count.fedex");
+    }
 
     @Traced
     public void send(Order order) {
@@ -22,6 +32,7 @@ public class FedexOrderPackageSender {
         } catch (InterruptedException e) {
             // ommited
         }
+        counter.increment();
     }
 
 }
--- src/main/java/com/redhat/quarkus/sre/label/sender/UPSOrderPackageSender.java
+++ src/main/java/com/redhat/quarkus/sre/label/sender/UPSOrderPackageSender.java
@@ -7,6 +7,8 @@ import com.redhat.quarkus.sre.label.domain.Order;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.opentracing.Traced;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 @ApplicationScoped
 @Traced
 public class UPSOrderPackageSender {
@@ -14,12 +16,21 @@ public class UPSOrderPackageSender {
     @ConfigProperty(name = "ups.delay", defaultValue = "4000")
     Integer delay;
 
+
+    private io.micrometer.core.instrument.Counter counter;
+
+    UPSOrderPackageSender(MeterRegistry registry) {
+        this.counter = registry.counter("sre.label-generator.count.ups");
+    }
+
+
     public void send(Order order) {
         try {
             Thread.sleep(delay);
         } catch (InterruptedException e) {
             // ommited
         }
+        counter.increment();
     }
 
 }
