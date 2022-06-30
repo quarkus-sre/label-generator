7ea67d260b1bb060fd4757745c4810c63e8428a7
Gabriel Sampaio <gsampaio@redhat.com>
2022-06-30 13:56:05 -0300

    fixing meter id

--- src/main/java/com/redhat/quarkus/integration/label/config/CustomConfiguration.java
+++ src/main/java/com/redhat/quarkus/integration/label/config/CustomConfiguration.java
@@ -1,6 +1,5 @@
 package com.redhat.quarkus.integration.label.config;
 
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import javax.enterprise.inject.Produces;
@@ -25,7 +24,7 @@ public class CustomConfiguration {
         return new MeterFilter() {
             @Override
             public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
-                if(id.getName().equalsIgnoreCase("upsTimerRequest")) {
+                if(id.getName().contains("TimerRequest")) {
                     return DistributionStatisticConfig.builder()
                         .percentiles(0.5, 0.90, 0.95, 0.99)     // median, 90th, 95th, 99th percentile, not aggregable
                         .percentilesHistogram(true) // histogram buckets (e.g. prometheus histogram_quantile)
