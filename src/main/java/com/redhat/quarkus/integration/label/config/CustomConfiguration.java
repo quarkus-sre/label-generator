cd3f8415b1e3e1e23938f6dd871667326ddd65b4
Gabriel Sampaio <gsampaio@redhat.com>
2022-06-30 13:03:35 -0300

    fixing slos buckets

--- src/main/java/com/redhat/quarkus/integration/label/config/CustomConfiguration.java
+++ src/main/java/com/redhat/quarkus/integration/label/config/CustomConfiguration.java
@@ -1,6 +1,7 @@
 package com.redhat.quarkus.integration.label.config;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
@@ -11,6 +12,11 @@ import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 
 @Singleton
 public class CustomConfiguration {
+
+    private long slo2nanoseconds = TimeUnit.NANOSECONDS.convert(2, TimeUnit.SECONDS);
+    private long slo3nanoseconds = TimeUnit.NANOSECONDS.convert(3, TimeUnit.SECONDS);
+    private long slo4nanoseconds = TimeUnit.NANOSECONDS.convert(4, TimeUnit.SECONDS);
+    private long slo5nanoseconds = TimeUnit.NANOSECONDS.convert(5, TimeUnit.SECONDS);
 
     /** Enable histogram buckets for a specific timer */
     @Produces
@@ -23,7 +29,7 @@ public class CustomConfiguration {
                     return DistributionStatisticConfig.builder()
                         .percentiles(0.5, 0.90, 0.95, 0.99)     // median, 90th, 95th, 99th percentile, not aggregable
                         .percentilesHistogram(true) // histogram buckets (e.g. prometheus histogram_quantile)
-                        .serviceLevelObjectives(0.5, 1.0, 2.5, 5.0)
+                        .serviceLevelObjectives(slo2nanoseconds, slo3nanoseconds, slo4nanoseconds, slo5nanoseconds) // fixed buckets in nanoseconds
                         .build()
                         .merge(config);
                 }
