b090598596bf867d470e83c0a4a8c8215483135a
Gabriel Sampaio <gsampaio@redhat.com>
2022-06-30 14:28:52 -0300

    ms buckets

--- src/main/java/com/redhat/quarkus/integration/label/config/CustomConfiguration.java
+++ src/main/java/com/redhat/quarkus/integration/label/config/CustomConfiguration.java
@@ -12,10 +12,11 @@ import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 @Singleton
 public class CustomConfiguration {
 
-    private long slo2nanoseconds = TimeUnit.NANOSECONDS.convert(2, TimeUnit.SECONDS);
-    private long slo3nanoseconds = TimeUnit.NANOSECONDS.convert(3, TimeUnit.SECONDS);
-    private long slo4nanoseconds = TimeUnit.NANOSECONDS.convert(4, TimeUnit.SECONDS);
-    private long slo5nanoseconds = TimeUnit.NANOSECONDS.convert(5, TimeUnit.SECONDS);
+    private long slo300ms = TimeUnit.NANOSECONDS.convert(300, TimeUnit.MILLISECONDS);
+    private long slo400ms = TimeUnit.NANOSECONDS.convert(400, TimeUnit.MILLISECONDS);
+    private long slo500ms = TimeUnit.NANOSECONDS.convert(500, TimeUnit.MILLISECONDS);
+    private long slo1s = TimeUnit.NANOSECONDS.convert(1, TimeUnit.SECONDS);
+    private long slo2s = TimeUnit.NANOSECONDS.convert(2, TimeUnit.SECONDS);
 
     /** Enable histogram buckets for a specific timer */
     @Produces
@@ -28,7 +29,7 @@ public class CustomConfiguration {
                     return DistributionStatisticConfig.builder()
                         .percentiles(0.5, 0.90, 0.95, 0.99)     // median, 90th, 95th, 99th percentile, not aggregable
                         .percentilesHistogram(true) // histogram buckets (e.g. prometheus histogram_quantile)
-                        .serviceLevelObjectives(slo2nanoseconds, slo3nanoseconds, slo4nanoseconds, slo5nanoseconds) // fixed buckets in nanoseconds
+                        .serviceLevelObjectives(slo300ms, slo400ms, slo500ms, slo1s, slo2s) // fixed buckets in nanoseconds
                         .build()
                         .merge(config);
                 }
